e4ab362b1d5f92322cd74e647d60c73bc5003ab1
man <man@odds.team>
2020-10-06 14:05:45 +0700

    Edit card carouse

--- src/components/Home/LikeHome.vue
+++ src/components/Home/LikeHome.vue
@@ -6,15 +6,18 @@
       <div class="row">
         <div class="col-md-10 mx-auto">
           <h3>MMEMEMEMEMME LIKE</h3>
-
           <div
             id="myCarousel-card"
             class="carousel slide"
             data-ride="carousel"
             data-interval="0"
           >
-            <!-- Carousel indicators -->
-            <ol class="carousel-indicators">
+          <!-- Carousel indicators -->
+          <div class="col-md-6 col-sm-6 col-xs-6 hidden-xs">
+                <div class="controls pull-right">
+                    <a class="left fa fa-chevron-left btn btn-info " href="#myCarousel-card" data-slide="prev"></a>
+                    <a class="right fa fa-chevron-right btn btn-info" href="#myCarousel-card" data-slide="next"></a>
+            <ol class="carousel-indicators carouse carousel-card">
               <li
                 data-target="#myCarousel-card"
                 data-slide-to="0"
@@ -22,6 +25,9 @@
               ></li>
               <li data-target="#myCarousel-card" data-slide-to="1"></li>
             </ol>
+                </div>
+            </div>
+            <br />
             <!-- Wrapper for carousel items -->
             <div class="carousel-inner">
               <div class="carousel-item-card active">
@@ -128,22 +134,112 @@
                 </div>
               </div>
             </div>
-            <!-- Carousel controls -->
+            <div class="carousel-inner">
+              <div class="carousel-item-card active">
+                <div class="row">
+                  <div class="col-sm-4">
+                    <div class="thumb-wrapper-card">
+                      <div class="img-box">
+                        <img
+                          src="https://images.unsplash.com/photo-1509408895711-7eda6e83c637?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1844cab997dc2e5f1ce14e3b9cbd0bc1&auto=format&fit=crop&w=1500&q=80"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="thumb-content-card">
+                        <h4>London</h4>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nam eu sem tempor, varius
+                          quam.asdasasasdasadsadasddas asdsadasdasdsadasdasas
+                          asdasdasasdasdasdsadsdasd
+                        </p>
+                        <div class="cart-section">
+                          <div class="row">
+                            <div class="col-md-6 col-sm-12 col-xs-6 review">
+                              <br /> 
+                              <h5 class="detail-price">$ 187.87</h5>
+                            </div>
+                            <div class="col-md-6 col-sm-12 col-xs-6 review-btn"> 
+                              <button href="#" class="btn btn-outline-shop"
+                                >Shop Now</button
+                              >                            
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-            <a
-              class="carousel-control-prev"
-              href="#myCarousel-card"
-              data-slide="prev"
-            >
-              <i class="fa fa-angle-left"></i>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#myCarousel-card"
-              data-slide="next"
-            >
-              <i class="fa fa-angle-right"></i>
-            </a>
+                  <div class="col-sm-4">
+                    <div class="thumb-wrapper-card">
+                      <div class="img-box">
+                        <img
+                          src="https://images.unsplash.com/photo-1509408895711-7eda6e83c637?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1844cab997dc2e5f1ce14e3b9cbd0bc1&auto=format&fit=crop&w=1500&q=80"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="thumb-content-card">
+                        <h4>London</h4>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nam eu sem tempor, varius
+                          quam.asdasasasdasadsadasddas asdsadasdasdsadasdasas
+                          asdasdasasdasdasdsadsdasd
+                        </p>
+                        <div class="cart-section">
+                          <div class="row">
+                            <div class="col-md-6 col-sm-12 col-xs-6 review">
+                              <br /> 
+                              <h5 class="detail-price">$ 187.87</h5>
+                            </div>
+                            <div class="col-md-6 col-sm-12 col-xs-6 review-btn"> 
+                              <button href="#" class="btn btn-outline-shop"
+                                >Shop Now</button
+                              >                            
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-sm-4">
+                    <div class="thumb-wrapper-card">
+                      <div class="img-box">
+                        <img
+                          src="https://images.unsplash.com/photo-1509408895711-7eda6e83c637?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1844cab997dc2e5f1ce14e3b9cbd0bc1&auto=format&fit=crop&w=1500&q=80"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="thumb-content-card">
+                        <h4>London</h4>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nam eu sem tempor, varius
+                          quam.asdasasasdasadsadasddas asdsadasdasdsadasdasas
+                          asdasdasasdasdasdsadsdasd
+                        </p>
+                        <div class="cart-section">
+                          <div class="row">
+                            <div class="col-md-6 col-sm-12 col-xs-6 review">
+                              <br /> 
+                              <h5 class="detail-price">$ 187.87</h5>
+                            </div>
+                            <div class="col-md-6 col-sm-12 col-xs-6 review-btn"> 
+                              <button href="#" class="btn btn-outline-shop"
+                                >Shop Now</button
+                              >                            
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Carousel controls -->
           </div>
         </div>
       </div>
