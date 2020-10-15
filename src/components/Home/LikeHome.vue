954a1e87af3afe8d78a49b6e4204da38ea9657fb
man <man@odds.team>
2020-10-15 16:09:14 +0700

    edite style carouselhome likehome and view likehome

--- src/components/Home/LikeHome.vue
+++ src/components/Home/LikeHome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-carousel">
     <div class="row">
-      <div class="col-md-12 ">
+      <div class="col-md-12">
         <div
           id="myCarouselcard"
           class="carousel slide"
@@ -11,15 +11,15 @@
         >
           <!-- Carousel indicators -->
           <div class="row">
-          <h3 class="headlike">MMEMEMEMEMME LIKE</h3>
-          <div class="col-md-6 col-sm-6 col-xs-6 hidden-xs">
-            <div class="controls pull-right">
+            <h3 class="headlike">MMEMEMEMEMME LIKE</h3>
+            <div class="col-md-6 col-sm-6 col-xs-6 hidden-xs">
+              <div class="controls pull-right">
                 <ol class="carouse carousel-card carousel-indicators">
-             <a
-                class="left fa fa-angle-left btn btn-info-card"
-                href="#myCarouselcard"
-                data-slide="prev"
-              ></a> 
+                  <a
+                    class="left fa fa-angle-left btn btn-info-card"
+                    href="#myCarouselcard"
+                    data-slide="prev"
+                  ></a>
                   <li
                     data-target="#myCarouselcard"
                     data-slide-to="0"
@@ -27,14 +27,14 @@
                   ></li>
                   <li data-target="#myCarouselcard" data-slide-to="1"></li>
                   <li data-target="#myCarouselcard" data-slide-to="2"></li>
-              <a
-                class="right fa fa-angle-right btn btn-info-card"
-                href="#myCarouselcard"
-                data-slide="next"
-              ></a>
+                  <a
+                    class="right fa fa-angle-right btn btn-info-card"
+                    href="#myCarouselcard"
+                    data-slide="next"
+                  ></a>
                 </ol>
+              </div>
             </div>
-          </div>
           </div>
           <br />
 
@@ -43,6 +43,73 @@
             <div class="carousel-item item-slide active">
               <div class="row">
                 <div class="col-sm-4">
+                  <div class="caption-thumb-wrapper-card">
+                  <div class="thumb-wrapper-card">
+                    <div class="img--box">
+                      <img
+                        src="https://images.unsplash.com/photo-1509408895711-7eda6e83c637?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1844cab997dc2e5f1ce14e3b9cbd0bc1&auto=format&fit=crop&w=1500&q=80"
+                        class="img-fluid"
+                        alt=""
+                      />
+                    </div>
+                    <div class="thumb-content-card">
+                      <h2 class="serif">
+                        Mob Psycho 100 OP / Opening Full - "99"
+                      </h2>
+                      <p class="text-card">
+                        The Hitch Hiker's Guide to the Galaxy has a few things
+                        to say on the subject of towels. A towel, it says, is
+                        about the most massivelyuseful thing an interstellar
+                        hitch hiker can have. Partly it has great practical
+                        value - you can wrap it around you for warmth as you
+                        bound across the cold moons of Jaglan Beta; you can lie
+                        on it on the brilliant marble-sanded beaches of
+                        Santraginus V, inhaling the heady sea vapours; you can
+                        sleep under it beneath the stars which shine so redly on
+                        the desert world of Kakrafoon; use it to sail a mini
+                        raft down the slow heavy river Moth; wet it for use in
+                        hand-to-hand-combat; wrap it round your head to ward off
+                        noxious fumes or to avoid the gaze of the Ravenous
+                        Bugblatter Beast of Traal (a mindboggingly stupid
+                        animal, it assumes that if you can't see it, it can't
+                        see you - daft as a bush, but very ravenous); you can
+                        wave your towel in emergencies as a distress signal, and
+                        of course dry yourself off with it if it still seems to
+                        be clean enough. More importantly, a towel has immense
+                        psychological value. For some reason, if a strag (strag:
+                        non-hitch hiker) discovers that a hitch hiker has his
+                        towel with him, he will automatically assume that he is
+                        also in possession of a toothbrush, face flannel, soap,
+                        tin of biscuits, flask, compass, map, ball of string,
+                        gnat spray, wet weather gear, space suit etc., etc.
+                        Furthermore, the strag will then happily lend the hitch
+                        hiker any of these or a dozen other items that the hitch
+                        hiker might accidentally have "lost". What the strag
+                        will think is that any man who can hitch the length and
+                        breadth of the galaxy, rough it, slum it, struggle
+                        against terrible odds, win through, and still knows
+                        where his towel is is clearly a man to be reckoned with.
+                      </p>
+                        <div class="cart-section">
+                          <div class="row">
+                            <div class="col-md-6 col-sm-12 col-xs-6 review">
+                              <h3 class="detail-price">$ 187.87</h3>
+                            </div>
+                            <div class="col-md-6 col-sm-12 col-xs-6 review-btn">
+                              <button class="btn btn-outline-shop">
+                                Shop Now
+                              </button>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>
+
+                <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -104,71 +171,11 @@
                     </div>
                   </div>
                 </div>
-
-                <div class="col-sm-4">
-                  <div class="thumb-wrapper-card">
-                    <div class="img--box">
-                      <img
-                        src="https://images.unsplash.com/photo-1509408895711-7eda6e83c637?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1844cab997dc2e5f1ce14e3b9cbd0bc1&auto=format&fit=crop&w=1500&q=80"
-                        class="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                    <div class="thumb-content-card">
-                      <h2 class="serif">
-                        Mob Psycho 100 OP / Opening Full - "99"
-                      </h2>
-                      <p class="text-card">
-                        The Hitch Hiker's Guide to the Galaxy has a few things
-                        to say on the subject of towels. A towel, it says, is
-                        about the most massivelyuseful thing an interstellar
-                        hitch hiker can have. Partly it has great practical
-                        value - you can wrap it around you for warmth as you
-                        bound across the cold moons of Jaglan Beta; you can lie
-                        on it on the brilliant marble-sanded beaches of
-                        Santraginus V, inhaling the heady sea vapours; you can
-                        sleep under it beneath the stars which shine so redly on
-                        the desert world of Kakrafoon; use it to sail a mini
-                        raft down the slow heavy river Moth; wet it for use in
-                        hand-to-hand-combat; wrap it round your head to ward off
-                        noxious fumes or to avoid the gaze of the Ravenous
-                        Bugblatter Beast of Traal (a mindboggingly stupid
-                        animal, it assumes that if you can't see it, it can't
-                        see you - daft as a bush, but very ravenous); you can
-                        wave your towel in emergencies as a distress signal, and
-                        of course dry yourself off with it if it still seems to
-                        be clean enough. More importantly, a towel has immense
-                        psychological value. For some reason, if a strag (strag:
-                        non-hitch hiker) discovers that a hitch hiker has his
-                        towel with him, he will automatically assume that he is
-                        also in possession of a toothbrush, face flannel, soap,
-                        tin of biscuits, flask, compass, map, ball of string,
-                        gnat spray, wet weather gear, space suit etc., etc.
-                        Furthermore, the strag will then happily lend the hitch
-                        hiker any of these or a dozen other items that the hitch
-                        hiker might accidentally have "lost". What the strag
-                        will think is that any man who can hitch the length and
-                        breadth of the galaxy, rough it, slum it, struggle
-                        against terrible odds, win through, and still knows
-                        where his towel is is clearly a man to be reckoned with.
-                      </p>
-                      <div class="cart-section">
-                        <div class="row">
-                          <div class="col-md-6 col-sm-12 col-xs-6 review">
-                            <h3 class="detail-price">$ 187.87</h3>
-                          </div>
-                          <div class="col-md-6 col-sm-12 col-xs-6 review-btn">
-                            <button href="#" class="btn btn-outline-shop">
-                              Shop Now
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
 
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -232,11 +239,14 @@
                 </div>
               </div>
             </div>
+            </div>
             <!-- <object 1> -->
 
             <div class="carousel-item item-slide">
               <div class="row">
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -298,8 +308,11 @@
                     </div>
                   </div>
                 </div>
+                </div>
 
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -360,8 +373,11 @@
                     </div>
                   </div>
                 </div>
+                </div>
 
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -423,6 +439,7 @@
                     </div>
                   </div>
                 </div>
+              </div>
               </div>
             </div>
             <!-- <object 2> -->
@@ -430,6 +447,8 @@
             <div class="carousel-item item-slide">
               <div class="row">
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -491,8 +510,11 @@
                     </div>
                   </div>
                 </div>
+                </div>
 
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -553,8 +575,11 @@
                     </div>
                   </div>
                 </div>
+                </div>
 
                 <div class="col-sm-4">
+                                    <div class="caption-thumb-wrapper-card">
+
                   <div class="thumb-wrapper-card">
                     <div class="img--box">
                       <img
@@ -617,6 +642,7 @@
                   </div>
                 </div>
               </div>
+            </div>
             </div>
             <!-- <object 3> -->
           </div>
